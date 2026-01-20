4f3a8494b14a9bfbb0cdd1c46ff466a4aa4b7931
Guy Sheffer <guysoft@gmail.com>
2026-01-20 21:45:58 +0200

    Swap volume up/down key positions in FN1 layer
    Swapped KC_VOLU and KC_VOLD positions so volume down is on the left
    and volume up is on the right, matching typical media key conventions.

--- keyboards/annepro2/keymaps/via/keymap.c
+++ keyboards/annepro2/keymaps/via/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_HOME, KC_END,  _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
-    _______,          KC_VOLU, KC_VOLD, KC_MUTE, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, KC_INS,  KC_DEL,  _______,
+    _______,          KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, KC_INS,  KC_DEL,  _______,
     _______, _______, _______,                            _______,                   _______, _______, MO(FN2), _______
 ),
  [FN2] = LAYOUT_60_ansi( /* FN2 */
